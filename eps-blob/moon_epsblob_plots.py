25664aed587581c056d5768f121ea189c11901e6
PanChengxi <panchengxi@baidu.com>
2024-04-20 11:19:30 +0800

    eps_blob

---
+++ eps-blob/moon_epsblob_plots.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import json
+
+# Load accuracy data from text files
+with open('./results/accs_GCN.txt', 'r') as file:
+    accs_GCN = json.load(file)
+
+with open('./results/accs_GIN.txt', 'r') as file:
+    accs_GIN = json.load(file)
+
+with open('./results/accs_GAT.txt', 'r') as file:
+    accs_GAT = json.load(file)
+
+# Extract epsilon values and corresponding accuracies
+eps_values = list(accs_GCN.keys())
+accs_values_GCN = list(accs_GCN.values())
+accs_values_GIN = list(accs_GIN.values())
+accs_values_GAT = list(accs_GAT.values())
+
+# Plot the accuracies
+plt.plot(eps_values, accs_values_GCN, label='GCN')
+plt.plot(eps_values, accs_values_GIN, label='GIN')
+plt.plot(eps_values, accs_values_GAT, label='GAT')
+
+# Add labels and title
+plt.xlabel('Epsilon')
+plt.ylabel('Accuracy')
+plt.title('Accuracy of Different Models')
+
+# Add legend
+plt.legend()
+
+# Show plot
+plt.grid(True)
+plt.savefig("./results/eps_blob_res.jpg")
+plt.show()
